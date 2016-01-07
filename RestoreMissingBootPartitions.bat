a66cf94054035f13e919941e8bc32c244d128ef2
Kyran Magpantay <magpantay.kyran@gmail.com>
2016-01-06 17:27:28 -0800

    Updated RestoreMissingBootPartitions.bat
    Testing the original program showed that it did not work completely. 
    Somewhat re-did the program and now it does what it is supposed to do.

--- RestoreMissingBootPartitions.bat
+++ RestoreMissingBootPartitions.bat
@@ -8,65 +8,108 @@ if /i {%isDeleted%}=={Y} (goto :yes)
 if /i {%isDeleted%}=={y} (goto :yes)
 if /i {%isDeleted%}=={N} (goto :no)
 if /i {%isDeleted%}=={n} (goto :no)
-if /i {%isDeleted%}=={exit} (goto :exit)
-if /i {%isDeleted%}=={bye} (goto :exit)
+if /i {%isDeleted%}=={exit} (goto :seeya)
+if /i {%isDeleted%}=={bye} (goto :seeya)
 goto :incorrectChoice
 
 :yes
 echo Creating partitions, please wait...
+goto :diskprt
 
-diskpart
-list disk
+:no
+set /p needsToBeDeleted="Do the partitions need to be deleted? (Y/N): "
+if /i {%needsToBeDeleted%}=={Y} (goto :needsToBeDeleted)
+if /i {%needsToBeDeleted%}=={y} (goto :needsToBeDeleted)
+if /i {%needsToBeDeleted%}=={N} (goto :logicCheck)
+if /i {%needsToBeDeleted%}=={n} (goto :logicCheck)
+if /i {%needsToBeDeleted%}=={exit} (goto :seeya)
+if /i {%needsToBeDeleted%}=={bye} (goto :seeya)
+goto :incorrectChoice
+
+:needsToBeDeleted
+echo >diskpartDel.txt LIST DISK
+diskpart /s diskpartDel.txt
 set /p diskNumber="Type in disk number (typically 0): "
-select disk %diskNumber%
-create partition efi size=100
-list partition
-format quick fs=fat32 label="System"
-assign letter=P
-create partition msr size=128
-list partition
-list vol
+echo >>diskpartDel.txt Select disk %diskNumber%
+echo >>diskpartDel.txt LIST PART
+diskpart /s diskpartDel.txt
+set /p partitionSys="Type in partition number that has the name System and is usually approx. 100-250MB large: "
+if /i {%partitionSys%}=={exit} (goto :diskprt)
+echo >diskpartDel2.txt sel part %partitionSys%
+echo >>diskpartDel2.txt del part override
+diskpart /s diskpartDel2.txt
+set /p partitionRes="Type in partition number that has the name Reserved and is usually approx. 128MB large: "
+if /i {%partitionRes%}=={exit} (goto :diskprt)
+echo >diskpartDel3.txt sel part %partitionRes%
+echo >>diskPartDel3.txt del part override
+diskpart /s diskpartDel3.txt
+goto :diskprt
+
+
+:diskprt
+echo >diskpart.txt LIST DISK
+diskpart /s diskpart.txt
+set /p diskNumber="Type in disk number (typically 0): "
+echo >>diskpart.txt Select disk %diskNumber%
+echo >>diskpart.txt create partition efi size=100
+echo >>diskpart.txt format quick fs=fat32 label="System"
+echo >>diskpart.txt assign letter=P
+echo >>diskpart.txt create partition msr size=128
+echo >>diskpart.txt LIST VOL
+diskpart /s diskpart.txt
 set /p volumeNumber="Type in the volume number that C is located in (typically 3): "
-select vol %volumeNumber%
-assign letter=C
-exit
+echo >diskpart2.txt Select vol %volumeNumber%
+echo >>diskpart2.txt assign letter=C
+echo >>diskpart2.txt exit
+diskpart /s diskpart2.txt
+pause
+goto :copyEFIfiles
 
+:copyEFIfiles
 echo Copying EFI files, please wait...
-
 mkdir P:\EFI\Microsoft\Boot
 xcopy /s C:\Windows\Boot\EFI\*.* P:\EFI\Microsoft\Boot
+pause
+goto :bcdcreate
 
+:bcdcreate
 echo Creating BCD store, please wait...
-
 P:
 cd EFI\Microsoft\Boot
 bcdedit /createstore BCD
-bcdedit /store BCD  /create {bootmgr} /d “Windows Boot Manager”
-bcdedit /store BCD /create /d “Windows 10” /application osloader
+bcdedit /store BCD /create {bootmgr} /d "Windows Boot Manger"
+bcdedit /store BCD /create /d "Windows 10" /application osloader
 set /p guidNumber="Input the GUID number just outputted by the previous command (including {}): "
-bcdedit /store BCD /set {bootmgr} default guidNumber
+bcdedit /store BCD /set {bootmgr} default %guidNumber%
 bcdedit /store BCD /set {bootmgr} path \EFI\Microsoft\Boot\bootmgfw.efi
 bcdedit /store BCD /set {bootmgr} displayorder {default}
 bcdedit /store BCD /set {default} device partition=c:
 bcdedit /store BCD /set {default} osdevice partition=c:
 bcdedit /store BCD /set {default} path \Windows\System32\winload.efi
 bcdedit /store BCD /set {default} systemroot \Windows
-
 echo Done.
-pause
-exit
-
-:no
-echo Partition deleting features will be available at a later time. Please delete the partitions first before proceeding.
-pause
-exit
+goto :seeya
 
 :incorrectChoice
 echo You have typed in an invalid choice.
-pause
-goto :beginning
+set /p wantToRestart="Would you like to start again? (Y/N): "
+if /i {%wantToRestart%}=={Y} (goto :beginning)
+if /i {%wantToRestart%}=={y} (goto :beginning)
+if /i {%wantToRestart%}=={N} (goto :seeya)
+if /i {%wantToRestart%}=={n} (goto :seeya)
+echo I will take that as a no...
+goto :seeya
 
-:exit
+:logicCheck
+echo Why are you even here then? It seems like you don't need this program.
+goto :seeya
+
+:seeya
+if EXIST diskpart.txt (del diskpart.txt)
+if EXIST diskpart2.txt (del diskpart2.txt)
+if EXIST diskpartDel.txt (del diskpartDel.txt)
+if EXIST dispartDel2.txt (del diskpartDel2.txt)
+if EXIST diskpartDel3.txt (del diskpartDel3.txt)
 echo Goodbye.
-pause
-exit
+exit /b 0
+
