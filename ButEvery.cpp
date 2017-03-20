4fed12b4892204dd89aba3e6d476a64bc79099be
Kyran Magpantay <magpantay.kyran@gmail.com>
2017-03-20 14:48:40 -0700

    Minor bug fixes and added features
    - Removed bug where there are a mixture of spaces and underscores in the resulting filename
    - Added a checkExtension function, which checks if the input files are a certain type of file before continuing
    - Improved on error checking and error throwing procedures for some functions
    - No longer need the user to enter "FILENAME" to indicate that a second file input is necessary, streamlined for the user
    - Code spacing fixes, for a e s t h e t i c reasons :)
    
    Known bugs:
    - More than one instance of the butEvery string in a line of words will only change the first instance of the word, but not future instances of that word on the same line
        - Using "fileIn >> string" rather than "getline(fileIn, string)" relives this, but produces more bugs, such as a lack of endlines (meaning all of the words just separated by a space rather than keeping the original formatting) and the first word will never get changed.

--- ButEvery.cpp
+++ ButEvery.cpp
@@ -5,221 +5,286 @@
 
 using namespace std;
 
+void removeSpaces(string & fileOutName)
+{
+        for (int i = 0; i < fileOutName.length(); i++)
+        {
+                if (fileOutName[i] == ' ')
+                {
+                        fileOutName[i] = '_';
+                }
+        }
+}
+
+bool checkExtension(string filename)
+{
+        return (filename.substr(filename.length()-4, 4) == ".txt");
+}
+
 void outsideFile(string filename, string & finalProduct)
 {
-  ifstream IRWFN; //Is Replaced With File Name
-  string buffer;
-  IRWFN.open(filename.c_str());
-
-  if (!IRWFN)
-  {
-    throw "Non-existent file! Try again.\n";
-  }
-  finalProduct = "";
-  while (!(IRWFN.eof()))
-  {
-    getline(IRWFN, buffer);
-    finalProduct += buffer;
-    finalProduct += "\n";
-  }
-  IRWFN.close();
+        ifstream IRWFN; //Is Replaced With File Name
+        string buffer;
+        IRWFN.open(filename.c_str());
+        /*if (!IRWFN)
+           {
+                throw "Non-existent file! Try again.\n";
+           }
+           else if ((!checkExtension(filename)))
+           {
+                throw "Incorrect file type!\n";
+           }
+           else
+           {*/
+        finalProduct = "";
+        while (!(IRWFN.eof()))
+        {
+                getline(IRWFN, buffer);
+                finalProduct += buffer;
+                finalProduct += "\n";
+        }
+        IRWFN.close();
+        //}
 }
 
 int matching (vector <string> & currentInput, string & butEvery, string & isReplacedWith)
 {
-  int counter = 0;
-  int CI_letter_traversal = 0;
-  int BE_traversal = 0;
-  for (int CI_word_traversal = 0; CI_word_traversal < currentInput.size(); CI_word_traversal++)
-  {
-      if (currentInput[CI_word_traversal].length() < butEvery.length())
-      {
-          continue; //pointless to run current iteration, as the length of butEvery exceeds length of input string
-      }
-      if (currentInput[CI_word_traversal] == butEvery)
-      {
-        currentInput[CI_word_traversal] = isReplacedWith;
-        counter++;
-        continue;
-      }
-      while (CI_letter_traversal < currentInput[CI_word_traversal].length()) //while it's not the end of currentInput[CI_word_traversal]
-      {
-            if (BE_traversal == butEvery.length()) //if the BE traversal variable matches the length of the butEvery string
-            {
-                counter++; //counting matches
+        int counter = 0;
+        int CI_letter_traversal = 0;
+        int BE_traversal = 0;
+        for (int CI_word_traversal = 0; CI_word_traversal < currentInput.size(); CI_word_traversal++)
+        {
+                if (currentInput[CI_word_traversal].length() < butEvery.length())
+                {
+                        continue; //pointless to run current iteration, as the length of butEvery exceeds length of input string
+                }
+                if (currentInput[CI_word_traversal] == butEvery)
+                {
+                        currentInput[CI_word_traversal] = isReplacedWith;
+                        counter++;
+                        continue;
+                }
+                while (CI_letter_traversal < currentInput[CI_word_traversal].length()) //while it's not the end of currentInput[CI_word_traversal]
+                {
+                        if (BE_traversal == butEvery.length()) //if the BE traversal variable matches the length of the butEvery string
+                        {
+                                counter++; //counting matches
 
-                string complete;
-                string substring_before = currentInput[CI_word_traversal].substr(0,CI_letter_traversal-butEvery.length());
-                string substring_after = currentInput[CI_word_traversal].substr(CI_letter_traversal, currentInput[CI_word_traversal].length()-CI_letter_traversal-1);
+                                string complete;
+                                string substring_before = currentInput[CI_word_traversal].substr(0,CI_letter_traversal-butEvery.length());
+                                string substring_after = currentInput[CI_word_traversal].substr(CI_letter_traversal, currentInput[CI_word_traversal].length()-CI_letter_traversal-1);
 
-                complete.append(substring_before);
-                complete.append(isReplacedWith);
-                complete.append(substring_after);
+                                complete.append(substring_before);
+                                complete.append(isReplacedWith);
+                                complete.append(substring_after);
 
-                currentInput[CI_word_traversal] = complete;
-                break;
-                //make substring from 0 to before all of the characters matched
-                //make a string of the isReplacedWith replace last butEvery.length() characters with isReplacedWith
-                //make a string of the rest of the characters after the match (thus removing the match)
-                //append substring1 and isReplacedWith
-                //append that with substring2
-            }
+                                currentInput[CI_word_traversal] = complete;
+                                break;
+                                //make substring from 0 to before all of the characters matched
+                                //make a string of the isReplacedWith replace last butEvery.length() characters with isReplacedWith
+                                //make a string of the rest of the characters after the match (thus removing the match)
+                                //append substring1 and isReplacedWith
+                                //append that with substring2
+                        }
 
-            char currentLetterInInput = tolower(currentInput[CI_word_traversal][CI_letter_traversal]);
-            char currentLetterInButEvery = tolower(butEvery[BE_traversal]); //lowercase each letter to check if each word matches, even with variances
+                        char currentLetterInInput = tolower(currentInput[CI_word_traversal][CI_letter_traversal]);
+                        char currentLetterInButEvery = tolower(butEvery[BE_traversal]); //lowercase each letter to check if each word matches, even with variances
 
-            if (currentLetterInInput == currentLetterInButEvery) //basic character matching
-            {
-                CI_letter_traversal++; //next character in currentInput[][]
-                BE_traversal++; //next character in butEvery
-            }
-            else
-            {
-                BE_traversal = 0; //start over butEvery again
-                CI_letter_traversal++; //but continue traversing currentInput[][]
-            }
-      }
-      CI_letter_traversal = 0; // reset for next iteration
-      BE_traversal = 0; //reset for next iteration
-  }
-  return counter;
+                        if (currentLetterInInput == currentLetterInButEvery) //basic character matching
+                        {
+                                CI_letter_traversal++; //next character in currentInput[][]
+                                BE_traversal++; //next character in butEvery
+                        }
+                        else
+                        {
+                                BE_traversal = 0; //start over butEvery again
+                                CI_letter_traversal++; //but continue traversing currentInput[][]
+                        }
+                }
+                CI_letter_traversal = 0; // reset for next iteration
+                BE_traversal = 0; //reset for next iteration
+        }
+        return counter;
 }
 
 void output(vector <string> & inputs, string filename)
 {
-  ofstream fileOut;
-  fileOut.open(filename.c_str());
-  for (int i = 0; i < inputs.size(); i++)
-  {
-      fileOut << inputs[i] << endl; //might as well output to file as well, needs endl here since '\n' doesn't translate well into text files
-      cout << inputs[i]; //no need for endl here, since there are '\n' in the string vector
-  }
-  fileOut << endl;
-  cout << endl;
-  fileOut.close();
+        ofstream fileOut;
+        fileOut.open(filename.c_str());
+        for (int i = 0; i < inputs.size(); i++)
+        {
+                fileOut << inputs[i] << endl; //might as well output to file as well, needs endl here since '\n' doesn't translate well into text files
+                cout << inputs[i]; //no need for endl here, since there are '\n' in the string vector
+        }
+        fileOut << endl;
+        cout << endl;
+        fileOut.close();
 }
 
-void prompts_and_inputs(string & filename, string & except, string & replaced, bool & isAFile)
+bool prompts_and_inputs(string & filename, string & except, string & replaced, bool & isAFile)
 {
-  cout << "Enter the file for input: ";
-  getline(cin, filename);
+        cout << "Enter the file for input: ";
+        getline(cin, filename);
 
-  ifstream fileIn;
-  fileIn.open(filename.c_str()); //need to convert it into a C string to have a variable open a file
-  if (!fileIn) //check if file is possible to open
-  {
-    throw "Non-existent file! Try again.\n";
-  }
+        ifstream fileIn;
+        fileIn.open(filename.c_str()); //need to convert it into a C string to have a variable open a file
+        if (!fileIn) //check if file is possible to open
+        {
+                throw 1;
+        }
+        else if (!(checkExtension(filename)))
+        {
+                throw 3;
+        }
+        else
+        {
+                cout << "Enter the phrase you want to replace: ";
+                getline(cin, except);
 
-  cout << "Enter the phrase you want to replace: ";
-  getline(cin, except);
+                cout << "Enter the phrase you want to replace \"" << except << "\" with: ";
+                getline(cin, replaced);
 
-  cout << "Enter the phrase you want to replace \"" << except << "\" with (Enter \"FILENAME\" for file): ";
-  getline(cin, replaced);
-
-  if (replaced == "FILENAME")
-  {
-      isAFile = true;
-  }
-
-  fileIn.close();
+                ifstream foreignFile;
+                foreignFile.open(replaced.c_str());
+                if (foreignFile)
+                {
+                        if (!(checkExtension(replaced)))
+                        {
+                                throw 3;
+                        }
+                        foreignFile.close();
+                        fileIn.close();
+                        return true;
+                }
+                else if (!foreignFile && checkExtension(replaced)) //the file ends in a .txt, but it does not exist
+                {
+                        throw 1;
+                }
+                else
+                {
+                        fileIn.close();
+                        foreignFile.close();
+                        return false;
+                }
+        }
 }
 
 int main ()
 {
-  string fileInName = "";
-  string fileOutName = "";
-  string butEvery = "";
-  string isReplacedWith = "";
-  bool replacedIsAFile = false;
+        string fileInName = "";
+        string fileOutName = "";
+        string butEvery = "";
+        string isReplacedWith = "";
+        bool replacedIsAFile = false;
 
-  int retry_count = 0;
-  bool success = false;
-  while (!success && retry_count <= 3) //if the user enters a wrong file, it will try again three times.
-  {
-    try
-    {
-        prompts_and_inputs(fileInName, butEvery, isReplacedWith, replacedIsAFile);
-        success = true;
-    }
-
-    catch(const char * exception)
-    {
-        retry_count++;
-        if (retry_count < 3)
+        int retry_count = 0;
+        bool success = false;
+        while (!success && retry_count <= 3) //if the user enters a wrong file, it will try again three times.
         {
-            cout << exception;
+                try
+                {
+                        replacedIsAFile = prompts_and_inputs(fileInName, butEvery, isReplacedWith, replacedIsAFile);
+                        success = true;
+                }
+
+                catch(int exception)
+                {
+                        if (exception == 1)
+                        {
+                                retry_count++;
+                                if (retry_count < 3)
+                                {
+                                        cout << "WARNING " << exception << "." << retry_count << ": Non-existent file! Try again.\n";
+                                }
+
+                                else
+                                {
+                                        cout << "ERROR: " << exception << ": Non-existent file!" << endl;
+                                        return exception;
+                                }
+                        }
+                        else if (exception == 3) //no idea why I skipped 2
+                        {
+                                cout << "ERROR " << exception << ": Filetype not supported. Accepted file types are: .txt" << endl;
+                                return exception;
+                        }
+                        else
+                        {
+                                cout << "ERROR " << exception << ": UNKNOWN ERROR" << endl;
+                                return exception;
+                        }
+                }
         }
 
-        else
+        if (replacedIsAFile)
         {
-            cout << "Non-existent file!" << endl;
-            return 1;
-        }
-    }
-  }
+                string isReplacedWithFN = isReplacedWith; //since isReplacedWith contains the filename
+                isReplacedWith = ""; //blank out the string
+                outsideFile(isReplacedWithFN, isReplacedWith);
+                /*success = false;
+                   retry_count = 0;
+                   while (!success && retry_count <= 3) //if the user enters a wrong file, it will try agian three times.
+                   {
+                        try
+                        {
+                                cout << "What is the text file name?: ";
+                                getline(cin, isReplacedWithFN);
+                                outsideFile(isReplacedWithFN, isReplacedWith);
+                                success = true;
+                        }
 
-  if (replacedIsAFile)
-  {
-      string isReplacedWithFN;
-      success = false;
-      retry_count = 0;
-      while (!success && retry_count <= 3) //if the user enters a wrong file, it will try agian three times.
-      {
-        try
+                        catch(const char * exception)
+                        {
+                                if (exception == "Incorrect file type!\n")
+                                {
+                                        cout << exception;
+                                        return 1;
+                                }
+                                retry_count++;
+                                if (retry_count < 3)
+                                {
+                                        cout << exception;
+                                }
+                                else
+                                {
+                                        cout << "Non-existent file!" << endl;
+                                        return 1;
+                                }
+                        }
+                   }*/
+                fileOutName = fileInName.substr(0, fileInName.length()-4) + "_only_every_" + butEvery + "_is_replaced_with_" + isReplacedWithFN.substr(0, isReplacedWithFN.length()-4) + ".txt";
+                removeSpaces(fileOutName); //removes the spaces, replaces them with underscores
+                //necessary to create a substring to remove the extension (assuming it is a three character extension)
+        }
+
+        ifstream fileIn;
+        fileIn.open(fileInName.c_str());
+
+        vector <string> currentInput; //because dynamically allocated string arrays couldn't cut it
+
+        string buffer;
+
+        while (!(fileIn.eof()))
         {
-            cout << "What is the text file name?: ";
-            getline(cin, isReplacedWithFN);
-            outsideFile(isReplacedWithFN, isReplacedWith);
-            success = true;
-        }
+                getline(fileIn, buffer);
+                currentInput.push_back(buffer);
+                currentInput.push_back("\n");
+        } //gets all of the words from the file
 
-        catch(const char * exception)
+        fileIn.close(); //no need for the file anymore
+
+        int match_count = matching(currentInput, butEvery, isReplacedWith);
+
+        if (!replacedIsAFile)
         {
-            retry_count++;
-            if (retry_count < 3)
-            {
-                cout << exception;
-            }
-
-            else
-            {
-                cout << "Non-existent file!" << endl;
-                return 1;
-            }
+                fileOutName = fileInName.substr(0, fileInName.length()-4) + "_only_every_" + butEvery + "_is_replaced_with_" + isReplacedWith + ".txt";
+                removeSpaces(fileOutName); //removes any spaces, leaves underscores instead
+                //necessary to create a substring to remove the extension (assuming it is a three character extension)
         }
-      }
-      fileOutName = fileInName.substr(0, fileInName.length()-4) + "_only_every_" + butEvery + "_is_replaced_with_" + isReplacedWithFN.substr(0, isReplacedWithFN.length()-4) + ".txt";
-      //necessary to create a substring to remove the extension (assuming it is a three character extension)
-  }
 
-  ifstream fileIn;
-  fileIn.open(fileInName.c_str());
+        output(currentInput, fileOutName);
 
-  vector <string> currentInput; //because dynamically allocated string arrays couldn't cut it
+        cout << "There were a total of " << match_count << " \"" << butEvery << "\" found in " << fileInName << endl;
 
-  string temp;
-
-  while (!(fileIn.eof()))
-  {
-      getline(fileIn, temp);
-      currentInput.push_back(temp);
-      currentInput.push_back("\n");
-  } //gets all of the words from the file
-
-  fileIn.close(); //no need for the file anymore
-
-  int match_count = matching(currentInput, butEvery, isReplacedWith);
-
-  if (!replacedIsAFile)
-  {
-      fileOutName = fileInName.substr(0, fileInName.length()-4) + "_only_every_" + butEvery + "_is_replaced_with_" + isReplacedWith + ".txt";
-      //necessary to create a substring to remove the extension (assuming it is a three character extension)
-  }
-
-  output(currentInput, fileOutName);
-
-  cout << "There were a total of " << match_count << " \"" << butEvery << "\" found in " << fileInName << endl;
-
-  return 0;
+        return 0;
 }
